d9b185531902701cf54882722d5f2eb1df8d10f0
CodeHamesh <work.hamesh@gmail.com>
2025-01-10 21:15:57 +0530

    add

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-
-function indianTime() {
-    return new Date().toLocaleTimeString("en-IN", { timeZone: "Asia/Kolkata" })
-}
 let hours = document.querySelector('.hours span');
 let min = document.querySelector('.min span');
 let sec = document.querySelector('.sec span');
 let ampm = document.querySelector('.am-pm');
-function time(fnc) {
-    let second = fnc().slice(6,9)
-    let minute = fnc().slice(3,5)
-    let houres = fnc().slice(0,2)
-    let am = fnc().slice(9,12).toUpperCase()
-    hours.textContent = houres
+function time() {
+    let second = new Date().getSeconds()
+    let minute = new Date().getMinutes()
+    let hourss = new Date().getHours()
+    let am = ""
+    if (hours >= 12) {
+        am = "pm"
+    }else{
+        am = "am"
+    }
+    hours.textContent = hourss
     min.textContent = minute
     sec.textContent = second   
     ampm.textContent = am
 }
 setInterval(()=>{
- time(indianTime)
+ time()
 },1000)
